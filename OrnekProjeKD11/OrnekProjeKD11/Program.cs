f5304c09d7277ac2cae249e22f6da85639823ec1
avnullahates <pana__anka@hotmail.com>
2022-06-10 12:06:05 +0300

    Ekrana hosgeldiniz eklemesi

--- OrnekProjeKD11/OrnekProjeKD11/Program.cs
+++ OrnekProjeKD11/OrnekProjeKD11/Program.cs
@@ -7,6 +7,17 @@ namespace OrnekProjeKD11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
+
+
+
+            string name = "Bilge Adam";
+            EkranaYaz(name);
+            Console.ReadLine();
+        }
+
+        private static void EkranaYaz(string name)
+        {
+            Console.WriteLine("Hosgeldiniz " + name);
         }
     }
 }
